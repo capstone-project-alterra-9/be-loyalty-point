b2bf142c35b2250f2d39a273b2bce0ea1a9c6d1c
Amzar123 <ajimuhammadzapar080500@gmail.com>
2022-11-17 22:36:18 +0700

    eddit gitignore

--- main.go
+++ main.go
@@ -1,49 +1,26 @@
 package main
 
 import (
+	"capstone-project/util"
 	"context"
 	"log"
-	_driverFactory "mini-project/drivers"
-	"mini-project/util"
-	"net/http"
+	_dbDriver "mini-project/drivers/mysql"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
 	"time"
-
-	// _userUseCase "mini-project/businesses/users"
-	// _userController "mini-project/controllers/users"
-
-	_studentUseCase "mini-project/businesses/students"
-	_studentController "mini-project/controllers/students"
-
-	_moduleUseCase "mini-project/businesses/modules"
-	_moduleController "mini-project/controllers/modules"
-
-	_teacherUseCase "mini-project/businesses/teachers"
-	_teacherController "mini-project/controllers/teachers"
-
-	_assignmentUsecase "mini-project/businesses/assignments"
-	_assignmentController "mini-project/controllers/assignments"
-
-	_dbDriver "mini-project/drivers/mysql"
-
-	_middleware "mini-project/app/middlewares"
-	_routes "mini-project/app/routes"
-
-	echo "github.com/labstack/echo/v4"
 )
 
 type operation func(ctx context.Context) error
 
 func main() {
 	configDB := _dbDriver.ConfigDB{
-		DB_USERNAME: "bffa768996b0ac",
-		DB_PASSWORD: "58d94e32",
-		DB_HOST:     "us-cdbr-east-06.cleardb.net",
-		DB_PORT:     "3306",
-		DB_NAME:     "heroku_706e7c18af76f4d",
+		DB_USERNAME: util.GetConfig("DB_USERNAME"),
+		DB_PASSWORD: util.GetConfig("DB_PASSWORD"),
+		DB_HOST:     util.GetConfig("DB_HOST"),
+		DB_PORT:     util.GetConfig("DB_PORT"),
+		DB_NAME:     util.GetConfig("DB_NAME"),
 	}
 
 	db := configDB.InitDB()
@@ -58,52 +35,6 @@ func main() {
 	configLogger := _middleware.ConfigLogger{
 		Format: "[${time_rfc3339}] ${status} ${method} ${host} ${path} ${latency_human}" + "\n",
 	}
-
-	e := echo.New()
-
-	studentRepo := _driverFactory.NewStudentRepository(db)
-	studentUsecase := _studentUseCase.NewStudentUsecase(studentRepo, &configJWT)
-	studentCtrl := _studentController.NewStudentController(studentUsecase)
-
-	moduleRepo := _driverFactory.NewModuleRepository(db)
-	moduleUsecase := _moduleUseCase.NewModuleUsecase(moduleRepo, &configJWT)
-	moduleCtrl := _moduleController.NewModuleController(moduleUsecase)
-
-	teacherRepo := _driverFactory.NewTeacherRepository(db)
-	teacherUsecase := _teacherUseCase.NewTeacherUsecase(teacherRepo, &configJWT)
-	teacherCtrl := _teacherController.NewTeacherController(teacherUsecase)
-
-	assignmentRepo := _driverFactory.NewAssignmentRepository(db)
-	assignmentUsecase := _assignmentUsecase.NewAssignmentUsecase(assignmentRepo, &configJWT)
-	assignmentCtrl := _assignmentController.NewAssignmentController(assignmentUsecase)
-
-	routesInit := _routes.ControllerList{
-		LoggerMiddleware:     configLogger.Init(),
-		JWTMiddleware:        configJWT.Init(),
-		StudentController:    *studentCtrl,
-		ModuleController:     *moduleCtrl,
-		TeacherController:    *teacherCtrl,
-		AssignmentController: *assignmentCtrl,
-	}
-
-	routesInit.RouteRegister(e)
-
-	go func() {
-		if err := e.Start(":80"); err != nil && err != http.ErrServerClosed {
-			e.Logger.Fatal("shutting down the server")
-		}
-	}()
-
-	wait := gracefulShutdown(context.Background(), 10*time.Second, map[string]operation{
-		"database": func(ctx context.Context) error {
-			return _dbDriver.CloseDB(db)
-		},
-		"http-server": func(ctx context.Context) error {
-			return e.Shutdown(context.Background())
-		},
-	})
-
-	<-wait
 }
 
 // gracefulShutdown performs application shut down gracefully.
