deacdd723b33e067a25d6c1836d464b0f1353b5b
ilhamfzz <ilhamfz117@gmail.com>
2022-11-25 15:15:26 +0700

    add base response

---
+++ dto/response.go
@@ -0,0 +1,31 @@
+package dto
+
+type Response struct {
+	Message string `json:"message"`
+	Errors  any    `json:"errors"`
+	Data    any    `json:"data"`
+}
+
+type EmptyObj struct{}
+
+func BuildResponse(message string, data any) Response {
+	return Response{
+		Message: message,
+		Errors:  EmptyObj{},
+		Data:    data,
+	}
+}
+
+func BuildErrorResponse(message string, err error) Response {
+	return Response{
+		Message: message,
+		Errors:  err.Error(),
+		Data:    EmptyObj{},
+	}
+}
+
+type Login struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Token    string `json:"token"`
+}
