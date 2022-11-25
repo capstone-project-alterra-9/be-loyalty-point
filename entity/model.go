47281fe67dee3649e3243f54e13b60ba9ae2203d
ilhamfzz <ilhamfz117@gmail.com>
2022-11-25 15:14:43 +0700

    set core entity

--- entity/model.go
+++ entity/model.go
@@ -3,3 +3,47 @@ package entity
 import "gorm.io/gorm"
 
 var DB *gorm.DB
+
+type Users struct {
+	gorm.Model
+	Username string `gorm:"type:varchar(20);not null;unique" json:"username"`
+	Email    string `gorm:"type:varchar(100);not null;unique" json:"email"`
+	Password string `gorm:"type:varchar(100);not null" json:"password"`
+	Points   int    `gorm:"type:int;not null" json:"points"`
+}
+
+type Products struct {
+	gorm.Model
+	Category  string `gorm:"type:varchar(20);not null" json:"category"`
+	Name      string `gorm:"type:varchar(100);not null" json:"name"`
+	Price     int    `gorm:"type:int;not null" json:"price"`
+	Stock     int    `gorm:"type:int;not null" json:"stock"`
+	Image     string `gorm:"type:varchar(200);not null" json:"image"`
+	CreatedBy string `gorm:"type:varchar(20);not null" json:"created_by"`
+	UpdatedBy string `gorm:"type:varchar(20);not null" json:"updated_by"`
+}
+
+type SerialNumbers struct {
+	gorm.Model
+	ProductID uint   `gorm:"type:int;not null" json:"product_id"`
+	Serial    string `gorm:"type:varchar(100);not null" json:"serial"`
+	Status    string `gorm:"type:varchar(20);not null" json:"status"`
+	CreatedBy string `gorm:"type:varchar(20);not null" json:"created_by"`
+}
+
+type Transactions struct {
+	gorm.Model
+	UserID        uint   `gorm:"type:int;not null" json:"user_id"`
+	ProductID     uint   `gorm:"type:int;not null" json:"product_id"`
+	SerialNumber  string `gorm:"type:varchar(100);not null" json:"serial_number"`
+	PhoneNumber   string `gorm:"type:varchar(20);not null" json:"phone_number"`
+	TotalPrice    int    `gorm:"type:int;not null" json:"total_price"`
+	LastUpdatedBy string `gorm:"type:varchar(20);not null" json:"last_updated_by"`
+}
+
+type Admins struct {
+	gorm.Model
+	Name     string `gorm:"type:varchar(100);not null" json:"name"`
+	Username string `gorm:"type:varchar(20);not null;unique" json:"username"`
+	Password string `gorm:"type:varchar(100);not null" json:"password"`
+}
