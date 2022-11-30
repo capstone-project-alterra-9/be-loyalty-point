01c61b352b27aea1bff70df9ad8b368d24464029
Amzar123 <ajimuhammadzapar080500@gmail.com>
2022-11-30 23:49:46 +0700

    BE-008 refactor code

--- route/route.go
+++ route/route.go
@@ -20,13 +20,23 @@ func New(Service service.Svc) *echo.Echo {
 	m.LogMiddleware(e)
 
 	// Routing withouth JWT
-	e.GET("/", controller.Connected)
-	e.POST("/api/login", controller.Login)
-	e.POST("/api/register", controller.Register)
+	// Routing withouth JWT
+	eApi := e.Group("/api")
+	eApi.POST("/login", controller.Login)
+	eApi.POST("/register", controller.Register)
 
 	eAuth := eApi.Group("/auth")
 	eAuth.Use(mid.JWT([]byte(os.Getenv("SECRET_JWT"))))
 	// Routing with JWT
+	eAuth.GET("/transactions", controller.GetTransactions)
+	eAuth.GET("/transactions/redeem", controller.GetTransactionsRedeem)
+	eAuth.GET("/transactions/buy", controller.GetTransactionsBuy)
+	eAuth.GET("/transactions/:id", controller.GetTransactionByID)
+	eAuth.GET("/history", controller.GetTransactionsByUser)
+	eAuth.POST("/transactions", controller.CreateTransactionByUser)
+	eAuth.POST("/transactions/dummy", controller.CreateTransactionByAdmin)
+	eAuth.PUT("/transactions/:id", controller.UpdateTransactionByAdmin)
+	eAuth.DELETE("/transactions/:id", controller.DeleteTransactionByAdmin)
 
 	// User endpoint
 	eAuth.DELETE("/users", controller.DeleteTransactionByAdmin)
