06a3e08d4f63496748063b39765e60e844de7f28
Amzar123 <ajimuhammadzapar080500@gmail.com>
2022-12-04 07:50:41 +0700

    BE-010 temp

--- controller/user.go
+++ controller/user.go
@@ -1,1 +1,17 @@
 package controller
+
+import (
+	"capstone-project/dto"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func DeleteOneById(c echo.Context) error {
+	id := c.Param("id")
+	user, err := Service.DeleteOneById(c, id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, dto.BuildErrorResponse("Failed to delete user", err))
+	}
+	return c.JSON(http.StatusOK, dto.BuildResponse("Success to delete user", user))
+} 
--- service/user.go
+++ service/user.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"capstone-project/entity"
+	jwtAuth "capstone-project/middleware"
+
+	"errors"
+	"github.com/labstack/echo/v4"
+)
+
+func (s *Service) DeleteOneById(c echo.Context, transactionID string) error {
+	user := jwtAuth.ExtractTokenUsername(c)
+	adminAuth, err := s.repo.GetAdminAuth(c, user)
+	if adminAuth != nil {
+		transactionDomain, err := s.repo.GetTransactionByID(c, transactionID)
+		if transactionDomain != nil {
+			err = s.repo.DeleteOneById(c, transactionID)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+		return err
+	}
+	return err
+}
